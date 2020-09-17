ef0ab20abbe6b3ca3e774932ee37a84d6c76c012
Patrick Rios <patrick.33.rios@hotmail.com>
2020-09-17 11:00:17 -0400

    Refactor sample to prepare for this app

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module RequireFX.App {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    opens main;
+    opens controller;
+    opens model;
+    opens view.css;
+    opens view.fxml;
+    opens view.images;
+}
