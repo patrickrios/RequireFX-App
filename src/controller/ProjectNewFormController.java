37927490f652046b43dc629e73fe232a12e58691
Patrick Rios <patrick.33.rios@hotmail.com>
2020-09-17 17:11:04 -0400

    exception label added to project form layout

--- src/controller/ProjectNewFormController.java
+++ src/controller/ProjectNewFormController.java
@@ -1,17 +1,33 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
 public class ProjectNewFormController {
     @FXML
     private AnchorPane anchorProjectNewForm;
+    @FXML
+    private TextField inputProjectName;
+    @FXML
+    private Label labelNameException;
 
     @FXML
     void closeForm() {
         StackPane form = (StackPane)anchorProjectNewForm.getParent();
         form.getChildren().remove(anchorProjectNewForm);
+    }
+
+    @FXML
+    void createProject() {
+        //TODO
+        //validar entrada
+        //montar objeto de modelo
+        //salvar no db
+        //informar usuário
+        labelNameException.setText("Já existe um projeto com este nome");
     }
 
 }
